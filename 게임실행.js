5ec07986a02d40665f3d69b697f9702948f91e19
DonnaKin <mkkim916@naver.com>
2017-12-25 21:27:05 +0900

    Add files via upload

---
+++ 게임실행.js
@@ -0,0 +1,309 @@
+setTimeout(function a(){
+  var i=1;
+  var time=setInterval(function b()
+  {document.getElementById("i1").style.top=(i+1)+'px';i++;
+          if(i==650){stop(); document.getElementById("i1").style.display="none"} }, 1);
+  function stop(){clearInterval(time);}}, 54000);
+
+setTimeout(function c(){
+  var j=1;
+  var time11=setInterval(function d()
+  {document.getElementById("i2").style.top=(j+1)+'px';j++;
+          if(j==550) {stop(); document.getElementById("i2").style.display="none"} }, 1);
+  function stop(){clearInterval(time11);}}, 3000);
+
+setTimeout(function e(){
+  var k=1;
+  var time2=setInterval(function f()
+  {document.getElementById("i3").style.top=(k+1)+'px';k++;
+          if(k==600) {stop(); document.getElementById("i3").style.display="none"} }, 1);
+  function stop(){clearInterval(time2);}}, 2000);
+
+setTimeout(function g(){
+  var l=1;
+  var time33=setInterval(function h()
+  {document.getElementById("i4").style.top=(l+1)+'px';l++;
+          if(l==550) {stop(); document.getElementById("i4").style.display="none"} }, 1);
+  function stop(){clearInterval(time33);}}, 5700);
+
+/*--------------------------------------------------------------------------------------------*/
+  setTimeout(function ah(){
+    var ih=1;
+    var time0=setInterval(function bh()
+    {document.getElementById("im1").style.top=(ih+1)+'px';ih++;
+            if(ih==650){stop(); document.getElementById("im1").style.display="none"} }, 1);
+    function stop(){clearInterval(time0);}}, 7800);
+
+  setTimeout(function ch(){
+    var jh=1;
+    var time1=setInterval(function dh()
+    {document.getElementById("im2").style.top=(jh+1)+'px';jh++;
+            if(jh==550) {stop(); document.getElementById("im2").style.display="none"} }, 1);
+    function stop(){clearInterval(time1);}}, 8300);
+
+  setTimeout(function eh(){
+    var kh=1;
+    var time2=setInterval(function fh()
+    {document.getElementById("im3").style.top=(kh+10)+'px';kh++;
+            if(kh==600) {stop(); document.getElementById("im3").style.display="none"} }, 1);
+    function stop(){clearInterval(time2);}}, 8450);
+
+  setTimeout(function gh(){
+    var lh=1;
+    var time3=setInterval(function hh()
+    {document.getElementById("im4").style.top=(lh+10)+'px';lh++;
+            if(lh==550) {stop(); document.getElementById("im4").style.display="none"} }, 1);
+    function stop(){clearInterval(time3);}}, 9700);
+
+  setTimeout(function oh(){
+    var mh=1;
+    var time4=setInterval(function ph()
+    {document.getElementById("im5").style.top=(mh+10)+'px';mh++;
+            if(mh== 550) {stop(); document.getElementById("im5").style.display="none"} }, 1);
+    function stop(){clearInterval(time4);}}, 10400);
+/*--------------------------------------------------------------------------------------------*/
+    setTimeout(function ag(){
+      var ig=1;
+      var time5=setInterval(function bg()
+      {document.getElementById("ima1").style.top=(ig+1)+'px';ig++;
+              if(ig==650){stop(); document.getElementById("ima1").style.display="none"} }, 1);
+      function stop(){clearInterval(time5);}}, 12300);
+
+    setTimeout(function cg(){
+      var jg=1;
+      var time6=setInterval(function dg()
+      {document.getElementById("ima2").style.top=(jg+1)+'px';jg++;
+              if(jg==550) {stop(); document.getElementById("ima2").style.display="none"} }, 1);
+      function stop(){clearInterval(time6);}}, 12400);
+
+    setTimeout(function eg(){
+      var kg=1;
+      var time7=setInterval(function fg()
+      {document.getElementById("ima3").style.top=(kg+1)+'px';kg++;
+              if(kg==600) {stop(); document.getElementById("ima3").style.display="none"} }, 1);
+      function stop(){clearInterval(time7);}}, 14800);
+
+    setTimeout(function gg(){
+      var lg=1;
+      var time8=setInterval(function hg()
+      {document.getElementById("ima4").style.top=(lg+1)+'px';lg++;
+              if(lg==550) {stop(); document.getElementById("ima4").style.display="none"} }, 1);
+      function stop(){clearInterval(time8);}}, 16300);
+
+    setTimeout(function og(){
+      var mg=1;
+      var time9=setInterval(function pg()
+      {document.getElementById("ima5").style.top=(mg+1)+'px';mg++;
+              if(mg==550) {stop(); document.getElementById("ima5").style.display="none"} }, 1);
+      function stop(){clearInterval(time9);}}, 17200);
+/*--------------------------------------------------------------------------------------------*/
+      setTimeout(function af(){
+        var ip=1;
+        var timeab=setInterval(function bf()
+        {document.getElementById("imag1").style.top=(ip+1)+'px';ip++;
+                if(ip==650){stop(); document.getElementById("imag1").style.display="none"} }, 1);
+        function stop(){clearInterval(timeab);}}, 20300);
+
+      setTimeout(function cf(){
+        var jf=1;
+        var timeaa=setInterval(function df()
+        {document.getElementById("imag2").style.top=(jf+1)+'px';jf++;
+                if(jf==550) {stop(); document.getElementById("imag2").style.display="none"} }, 1);
+        function stop(){clearInterval(timeaa);}}, 19200);
+
+      setTimeout(function ef(){
+        var kf=1;
+        var time2f=setInterval(function ff()
+        {document.getElementById("imag3").style.top=(kf+1)+'px';kf++;
+                if(kf==600) {stop(); document.getElementById("imag3").style.display="none"} }, 1);
+        function stop(){clearInterval(time2f);}}, 18500);
+
+      setTimeout(function gf(){
+        var lf=1;
+        var times=setInterval(function hf()
+        {document.getElementById("imag4").style.top=(lf+1)+'px';lf++;
+                if(lf==550) {stop(); document.getElementById("imag4").style.display="none"} }, 1);
+        function stop(){clearInterval(times);}}, 23000);
+
+      setTimeout(function of(){
+        var mf=1;
+        var timej=setInterval(function pf()
+        {document.getElementById("imag5").style.top=(mf+1)+'px';mf++;
+                if(mf==550) {stop(); document.getElementById("imag5").style.display="none"} }, 1);
+        function stop(){clearInterval(timej);}}, 24300);
+/*--------------------------------------------------------------------------------------------*/
+        setTimeout(function ae(){
+          var ie=1;
+          var timek=setInterval(function be()
+          {document.getElementById("image1").style.top=(ie+1)+'px';ie++;
+                  if(ie==550){stop(); document.getElementById("image1").style.display="none"} }, 1);
+          function stop(){clearInterval(timek);}}, 27000);
+
+        setTimeout(function ce(){
+          var je=1;
+          var timel=setInterval(function de()
+          {document.getElementById("image2").style.top=(je+1)+'px';je++;
+                  if(je==550) {stop(); document.getElementById("image2").style.display="none"} }, 1);
+          function stop(){clearInterval(timel);}}, 28100);
+
+        setTimeout(function ee(){
+          var ke=1;
+          var timem=setInterval(function fe()
+          {document.getElementById("image3").style.top=(ke+1)+'px';ke++;
+                  if(ke==600) {stop(); document.getElementById("image3").style.display="none"} }, 1);
+          function stop(){clearInterval(timem);}}, 26400);
+
+        setTimeout(function ge(){
+          var le=1;
+          var timen=setInterval(function he()
+          {document.getElementById("image4").style.top=(le+1)+'px';le++;
+                  if(le==550) {stop(); document.getElementById("image4").style.display="none"} }, 1);
+          function stop(){clearInterval(timen);}}, 33000);
+
+        setTimeout(function oe(){
+          var me=1;
+          var timeh=setInterval(function pe()
+          {document.getElementById("image5").style.top=(me+1)+'px';me++;
+                  if(me==550) {stop(); document.getElementById("image5").style.display="none"} }, 1);
+          function stop(){clearInterval(timeh);}}, 29350);
+/*--------------------------------------------------------------------------------------------*/
+        setTimeout(function ad(){
+          var id=1;
+          var timeu=setInterval(function b()
+          {document.getElementById("imge1").style.top=(id+1)+'px';id++;
+                  if(id==650){stop(); document.getElementById("imge1").style.display="none"} }, 1);
+          function stop(){clearInterval(timeu);}}, 32000);
+
+        setTimeout(function cd(){
+          var jd=1;
+          var timei=setInterval(function dd()
+          {document.getElementById("imge2").style.top=(jd+1)+'px';jd++;
+                  if(jd==550) {stop(); document.getElementById("imge2").style.display="none"} }, 1);
+          function stop(){clearInterval(timei);}}, 38100);
+
+        setTimeout(function ed(){
+          var kd=1;
+          var timeo=setInterval(function fd()
+          {document.getElementById("imge3").style.top=(kd+1)+'px';kd++;
+                 if(kd==550) {stop(); document.getElementById("imge3").style.display="none"} }, 1);
+          function stop(){clearInterval(timeo);}}, 36400);
+
+        setTimeout(function gd(){
+          var ld=1;
+          var timep=setInterval(function hd()
+          {document.getElementById("imge4").style.top=(ld+1)+'px';ld++;
+                  if(ld==550) {stop(); document.getElementById("imge4").style.display="none"} }, 1);
+          function stop(){clearInterval(timep);}}, 39100);
+
+        setTimeout(function od(){
+          var md=1;
+          var timev=setInterval(function pd()
+          {document.getElementById("imge5").style.top=(md+1)+'px';md++;
+                  if(md==550) {stop(); document.getElementById("imge5").style.display="none"} }, 1);
+          function stop(){clearInterval(timev);}}, 41800);
+/*--------------------------------------------------------------------------------------------*/
+        setTimeout(function ac(){
+          var ic=1;
+          var timey=setInterval(function bc()
+          {document.getElementById("mage1").style.top=(ic+1)+'px';ic++;
+                  if(ic==650){stop(); document.getElementById("mage1").style.display="none"} }, 1);
+          function stop(){clearInterval(timey);}}, 42500);
+
+        setTimeout(function cc(){
+          var jc=1;
+          var timez=setInterval(function dc()
+          {document.getElementById("mage2").style.top=(jc+1)+'px';jc++;
+                  if(jc==550) {stop(); document.getElementById("mage2").style.display="none"} }, 1);
+          function stop(){clearInterval(timez);}}, 43200);
+
+        setTimeout(function ec(){
+          var kc=1;
+          var timex=setInterval(function fc()
+          {document.getElementById("mage3").style.top=(kc+1)+'px';kc++;
+                  if(kc==550) {stop(); document.getElementById("mage3").style.display="none"} }, 1);
+          function stop(){clearInterval(timex);}}, 46800);
+
+        setTimeout(function gc(){
+          var lc=1;
+          var timeq=setInterval(function hc()
+          {document.getElementById("mage4").style.top=(lc+1)+'px';lc++;
+                  if(lc==550) {stop(); document.getElementById("mage4").style.display="none"} }, 1);
+          function stop(){clearInterval(timeq);}}, 42300);
+
+        setTimeout(function oc(){
+          var mc=1;
+          var timew=setInterval(function pc()
+          {document.getElementById("mage5").style.top=(mc+1)+'px';mc++;
+                if(mc==550) {stop(); document.getElementById("mage5").style.display="none"} }, 1);
+          function stop(){clearInterval(timew);}}, 45000);
+/*--------------------------------------------------------------------------------------------*/
+        setTimeout(function ab(){
+          var ib=1;
+          var timer=setInterval(function bb()
+          {document.getElementById("age1").style.top=(ib+1)+'px';ib++;
+                  if(ib==650){stop(); document.getElementById("age1").style.display="none"} }, 1);
+          function stop(){clearInterval(timer);}}, 49000);
+
+        setTimeout(function cb(){
+          var jb=1;
+          var timet=setInterval(function db()
+          {document.getElementById("age2").style.top=(jb+1)+'px';jb++;
+                if(jb==550) {stop(); document.getElementById("age2").style.display="none"} }, 1);
+          function stop(){clearInterval(timet);}}, 50300);
+
+        setTimeout(function eb(){
+          var kb=1;
+          var timeg=setInterval(function fb()
+          {document.getElementById("age3").style.top=(kb+1)+'px';kb++;
+                  if(kb==550) {stop(); document.getElementById("age3").style.display="none"} }, 1);
+          function stop(){clearInterval(timeg);}}, 48300);
+
+        setTimeout(function gb(){
+          var lb=1;
+          var timef=setInterval(function hb()
+          {document.getElementById("age4").style.top=(lb+1)+'px';lb++;
+                  if(lb==550) {stop(); document.getElementById("age4").style.display="none"} }, 1);
+          function stop(){clearInterval(timef);}}, 53500);
+
+        setTimeout(function ob(){
+          var mb=1;
+          var timee=setInterval(function pb()
+          {document.getElementById("age5").style.top=(mb+1)+'px';mb++;
+                  if(mb==550) {stop(); document.getElementById("age5").style.display="none"} }, 1);
+          function stop(){clearInterval(timee);}}, 47200);
+/*--------------------------------------------------------------------------------------------*/
+        setTimeout(function aa(){
+          var ia=1;
+          var timed=setInterval(function b()
+          {document.getElementById("ge1").style.top=(ia+1)+'px';ia++;
+                  if(ia==650){stop(); document.getElementById("ge1").style.display="none"} }, 1);
+          function stop(){clearInterval(timed);}}, 51000);
+
+        setTimeout(function ca(){
+          var ja=1;
+          var timec=setInterval(function da()
+          {document.getElementById("ge2").style.top=(ja+1)+'px';ja++;
+                  if(ja==550) {stop(); document.getElementById("ge2").style.display="none"} }, 1);
+          function stop(){clearInterval(timec);}}, 51240);
+
+        setTimeout(function ea(){
+          var ka=1;
+          var time2a=setInterval(function fa()
+          {document.getElementById("ge3").style.top=(ka+1)+'px';ka++;
+                  if(ka==550) {stop(); document.getElementById("ge3").style.display="none"} }, 1);
+          function stop(){clearInterval(time2a);}}, 52000);
+
+        setTimeout(function ga(){
+          var la=1;
+          var timeb=setInterval(function ha()
+          {document.getElementById("ge3").style.top=(la+1)+'px';la++;
+                  if(la==550) {stop(); document.getElementById("ge3").style.display="none"} }, 1);
+          function stop(){clearInterval(timeb);}}, 54000);
+
+        setTimeout(function oa(){
+          var ma=1;
+          var timea=setInterval(function pa()
+          {document.getElementById("ge4").style.top=(ma+1)+'px';ma++;
+                  if(ma==550) {stop(); document.getElementById("ge4").style.display="none"} }, 1);
+          function stop(){clearInterval(timea);}}, 53700);
+/*--------------------------------------------------------------------------------------------*/
